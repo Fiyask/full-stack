d2a0827c54f8078d28d02edae17166440d29bfb7
fiyask <fiyaszakeer@gmail.com>
2024-07-10 12:09:09 +0530

    Full -stack

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './header.css'
+
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1 className='header_right'>
+        W
+      </h1>
+      <ul className='header_left'>
+        <li>about</li>
+        <li>work</li>
+        <li>contact</li>
+
+      </ul>
+        
+
+    </div>
+  )
+}
+
+export default Header
---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./home.css"
+import Header from '../../components/Header/Header'
+import Experience from '../../components/Header/Experience'
+
+
+
+const Home = () => {
+  return (
+    <div> 
+        <Header />
+        <Experience />
+    </div>
+  )
+}
+
+export default Home
